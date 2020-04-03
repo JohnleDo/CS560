39bdb4a924958ea4f019bd786f7ad2e8dacc13ec
JohnleDo <kitsuneace@gmail.com>
2020-04-03 13:01:48 -0500

    Attempted to add the ability to count the amount of comparsions

--- maxima2D.py
+++ maxima2D.py
@@ -1,23 +1,26 @@
-def mergeSort(arr):
+def mergeSort(arr, key):
+    sortCount = 0
+
     if len(arr) > 1:
         mid = len(arr)//2  # Finding the mid of the array
         L = arr[:mid]  # Dividing the array elements
         R = arr[mid:]  # into 2 halves
 
-        mergeSort(L)  # Sorting the first half
-        mergeSort(R)  # Sorting the second half
+        sortCount += mergeSort(L, key)  # Sorting the first half
+        sortCount += mergeSort(R, key)  # Sorting the second half
 
         i = j = k = 0
 
         # Copy data to temp arrays L[] and R[]
         while i < len(L) and j < len(R):
-            if L[i] < R[j]:
+            if L[i][key] < R[j][key]:
                 arr[k] = L[i]
                 i += 1
             else:
                 arr[k] = R[j]
                 j += 1
             k += 1
+            sortCount += 1
 
         # Checking if any element was left
         while i < len(L):
@@ -29,6 +32,8 @@ def mergeSort(arr):
             arr[k] = R[j]
             j += 1
             k += 1
+
+    return sortCount
 
 
 def groupingPointList(output, pointListNumber):
@@ -45,10 +50,11 @@ def groupingPointList(output, pointListNumber):
 
         if i != pointValue:
             points.append({'Point List Number': pointListNumber,
-                           'x': output[x].split()[0],
-                           'y': output[x].split()[1],
+                           'Point Amount': pointValue,
+                           'x': float(output[x].split()[0]),
+                           'y': float(output[x].split()[1]),
                            'maximal': False,
-                           'where': i})
+                           'where': i + 1})
             i += 1
 
     del output[0:pointValue]
@@ -60,7 +66,9 @@ if __name__ == '__main__':
     filename = 'points1.txt'
     output = []
     pointList = []
+    sortCountList = []
     lock = True
+    x = 0
     pointGroupCounter = 0
 
     with open(filename, 'r') as f:
@@ -74,3 +82,7 @@ if __name__ == '__main__':
             pointGroupCounter += 1
         else:
             lock = False
+
+    while(x != len(pointList)):
+        sortCountList.append(mergeSort(pointList[x], 'x'))
+        x += 1
